d895dd91190fee7163d58370ec00f664b749437c
g-wong <g.wong1205@gmail.com>
2015-03-13 06:16:15 +0900

    Revert "Change log format"
    This reverts commit 6d2323c7a0d3bfa166fd829cd0a266b23625a79f.

--- accesslog2csv.pl
+++ accesslog2csv.pl
@@ -18,16 +18,12 @@ if ("$ARGV[0]" =~ /^-h|--help$/) {
 %MONTHS = ( 'Jan' => '01', 'Feb' => '02', 'Mar' => '03', 'Apr' => '04', 'May' => '05', 'Jun' => '06',
   'Jul' => '07', 'Aug' => '08', 'Sep' => '09', 'Oct' => '10', 'Nov' => '11', 'Dec' => '12' );
 
-print STDOUT "\"Host\",\"Log Name\",\"Date Time\",\"Time Zone\",\"Method\",\"URL\",\"Response Code\",\"Response Body\",\"Processing Time\"\n";
+print STDOUT "\"Host\",\"Log Name\",\"Date Time\",\"Time Zone\",\"Method\",\"URL\",\"Response Code\",\"Bytes Sent\",\"Referer\",\"User Agent\"\n";
 $line_no = 0;
 
 while (<>) {
   ++$line_no;
-  
-  $line = $_;
-  $line =~ s/\s\?/?/;
-  
-  if ($line =~ /^([\w\.:-]+)\s+([\w\.:-]+)\s+([\w\.-]+)\s+\[(\d+)\/(\w+)\/(\d+):(\d+):(\d+):(\d+)\s?([\w:\+-]+)]\s+"(\w+)\s+(\S+)\s+HTTP\/1\.\d"\s+(.*)/) {
+  if (/^([\w\.:-]+)\s+([\w\.:-]+)\s+([\w\.-]+)\s+\[(\d+)\/(\w+)\/(\d+):(\d+):(\d+):(\d+)\s?([\w:\+-]+)]\s+"(\w+)\s+(\S+)\s+HTTP\/1\.\d"\s+(\d+)\s+([\d-]+)((\s+"([^"]+)"\s+")?([^"]+)")?$/) {
     $host = $1;
     $other = $2;
     $logname = $3;
@@ -40,30 +36,16 @@ while (<>) {
     $tz = $10;
     $method = $11;
     $url = $12;
-    
-    $tmp = $13;
-    print "tmp = \"$tmp\"\n";
-    my $code = 0;
-    my $byters = 0;
-    my $ptime = 0;
-    if ( $tmp =~ /^(\d+)\s+([\d-]+)\s?$/ ){
-        $code = $1;
-        if ($14 eq '-') {
-          $byters = 0;
-        } else {
-          $byters = $2;
-        }
-    } elsif ( $tmp =~ /^(\d+)\s+([\d-]+)\s+([\d-]+)\s?$/ ){
-        $code = $1;
-        if ($14 eq '-') {
-          $byters = 0;
-        } else {
-          $byters = $2;
-        }
-        $ptime = $3;
+    $code = $13;
+    if ($14 eq '-') {
+      $bytesd = 0;
+    } else {
+      $bytesd = $14;
     }
+    $referer = $17;
+    $ua = $18;
 
-    print STDOUT "\"$host\",\"$logname\",\"$year-$month-$day $hour:$min:$sec\",\"GMT$tz\",\"$method\",\"$url\",$code,$byters,$ptime\n";
+    print STDOUT "\"$host\",\"$logname\",\"$year-$month-$day $hour:$min:$sec\",\"GMT$tz\",\"$method\",\"$url\",$code,$bytesd,\"$referer\"\,\"$ua\"\n";
   } else {
     print STDERR "Invalid Line at $line_no: $_";
   }
